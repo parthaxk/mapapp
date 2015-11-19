fcaf96a3e3aa6c7417de465ae54df90b53f347e9
g013001 <Mrinal.Pasari@gjensidige.no>
2015-11-19 19:39:12 +0530

    Dll issue FIx.

--- Pointmeout/App_Start/WebApiConfig.cs
+++ Pointmeout/App_Start/WebApiConfig.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using Newtonsoft.Json;
+using System.Net.Http.Formatting;
+using Newtonsoft.Json.Serialization;
 
 namespace Pointmeout
 {
@@ -11,20 +13,12 @@ namespace Pointmeout
 
         public static void Register(HttpConfiguration config)
         {
-            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().FirstOrDefault();
-            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().FirstOrDefault();
-            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-            // Web API configuration and services
-
-            // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().FirstOrDefault();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+           
+
         }
     }
 }
